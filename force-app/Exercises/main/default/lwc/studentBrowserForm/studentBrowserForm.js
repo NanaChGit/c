af6123e30b9fd424f2db0c2835015075c13f6b65
Nana Ch <changnaxx@163.com>
2022-05-19 00:08:46 +0800

    Exercise 2-8 Completed

--- force-app/Exercises/main/default/lwc/studentBrowserForm/studentBrowserForm.js
+++ force-app/Exercises/main/default/lwc/studentBrowserForm/studentBrowserForm.js
@@ -1,3 +1,58 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api,track,wire } from 'lwc';
+import getInstructors from '@salesforce/apex/StudentBrowserForm.getInstructors';
+import getDeliveriesByInstructor from '@salesforce/apex/StudentBrowserForm.getDeliveriesByInstructor'; 
+export default class StudentBrowserForm extends LightningElement {
+    //111111111111111111111111111-------------------------------------
+    @track instructors = [];
+    error; 
 
-export default class StudentBrowserForm extends LightningElement {}
+    @wire(getInstructors) 
+    wired_getInstructors({ error, data }) { 
+        this.instructors = []; 
+        if (data) { 
+            this.instructors.push({ 
+                value: '', 
+                label: 'Select an instructor' 
+            }); 
+            data.forEach(instructor => { 
+                this.instructors.push({ 
+                value: instructor.Id, 
+                label: instructor.Name 
+                }); 
+            }); 
+        } else if (error) { 
+        this.error = error; 
+        }
+    }
+
+  //22222222222222222222222222222222222222222222222222222222222222222222222
+    selectedInstructorId = ''; 
+    @track deliveries = [];
+    selectedDeliveryId = '';
+    @wire(getDeliveriesByInstructor, {instructorId : '$selectedInstructorId'})
+    wired_getDeliveriesByInstructor({ error, data }) { 
+	    this.deliveries = [];
+	    if (data && data.length) {
+		    if (this.selectedInstructorId) {
+			    this.deliveries.push({
+				    value: '',
+				    label: 'Any delivery'
+			    });
+		    }
+		    data.forEach(delivery => {
+			    this.deliveries.push({
+				    value: delivery.Id,
+				    label: `${delivery.Start_Date__c} ${delivery.Location__c} ${delivery.Attendee_Count__c} students`
+			    });
+		    });
+	    } else if (error) {
+		    this.error = error;
+	    }
+    }
+
+    //33333-------------------------------------------------
+    onInstructorChange(event) { 
+        this.selectedDeliveryId = ''; 
+        this.selectedInstructorId = event.target.value; 
+    }
+}
