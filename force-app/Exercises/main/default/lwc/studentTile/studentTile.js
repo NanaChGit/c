27fa4610cdc75a05859ad30d93b02f442b5673ef
Nana Ch <changnaxx@163.com>
2022-05-23 17:40:23 +0800

    Exercise 2-9 Contacts queried

--- force-app/Exercises/main/default/lwc/studentTile/studentTile.js
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -5,8 +5,17 @@ export default class StudentTile extends LightningElement {
         Name: 'Nana', 
         PhotoUrl: '/services/images/photo/003B0FakePictId',
     };
-    @api selected = false;
+   // @api selected = false;
     studentClick(){ 
-        alert(this.student.Name); 
+        //c/challenge_currentDateTimealert(this.student.Name); 
+        const evt = new CustomEvent('studentselected', { 
+            detail: { studentId: this.student.Id } 
+        }); 
+        this.dispatchEvent(evt);
     } 
+
+    @api selectedStudentId = ''; 
+    get tileSelected() { 
+        return (this.selectedStudentId===this.student.Id) ? "tile selected" : "tile"; 
+    }
 }
--- force-app/Exercises/main/default/lwc/studentTiles/studentTiles.js
+++ force-app/Exercises/main/default/lwc/studentTiles/studentTiles.js
@@ -1,6 +1,9 @@
-import { api } from 'lwc';
-import { LightningElement } from 'lwc';
+import { LightningElement ,api,track} from 'lwc';
 
 export default class StudentTiles extends LightningElement {
     @api studentList = []; 
+    @track selectedStudentId = '';
+    handleStudentSelected(event){ 
+        this.selectedStudentId=event.detail.studentId; 
+    }
 }
